b6553c81728f9a6ea98df3c6f61a3fa55322a5e6
Nikita Vybornov <nikitavybornov34@gmail.com>
2025-10-12 20:01:47 +0900

    Add locators to order page

--- Locators/order_page_locators.py
+++ Locators/order_page_locators.py
@@ -4,9 +4,15 @@ class OrderPageLocators:
     NAME_INPUT = (By.XPATH, '//input[contains (@placeholder, "Имя")]')
     LAST_NAME_INPUT = (By.XPATH, '//input[contains (@placeholder, "Фамилия")]')
     ADRESS_INPUT = (By.XPATH, '//input[contains (@placeholder, "Адрес")]')
-    #METRO_LIST = (By.CLASS_NAME, "metro-list") Пока с этим вопросы
+    METRO_LIST = (By.XPATH, f'//button[@value={value}]')
     PHONE_INPUT = (By.XPATH, '//input[contains (@placeholder, "Телефон")]')
     CONTINUE_BUTTON = (By.XPATH, '//button[text()="Далее"]')
     WHEN_INPUT = (By.XPATH, '//input[contains (@placeholder, "Когда")]')
+    ORDER_DATE = (By.XPATH, '//div[text()="трое суток"]')
+    VEHICLE_COLOR = (By.XPATH, '//input[@id="grey"]')
+    ORDER_FORM_BUTTON = (By.XPATH, "//div[contains(@class, 'Order')]//button[text()='Заказать']")
+    CONFIRM_BUTTON = (By.XPATH, "//div[contains(@class, 'Order')]//button[text()='Да']")
+    CONFIRM_WINDOW = (By.XPATH, "//div[text()='Заказ оформлен']")
+
 
 
--- curl.py
+++ curl.py
@@ -1,2 +1,3 @@
 main_site = "https://qa-scooter.praktikum-services.ru/"
 order_site = f"main_site{order}"
+dzen_page = 'https://dzen.ru/'
