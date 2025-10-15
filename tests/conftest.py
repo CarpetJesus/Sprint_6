60d840b3192ee81777579a5f8575166d22a43453
Nikita Vybornov <nikitavybornov34@gmail.com>
2025-10-15 12:48:21 +0900

    remove screen settings and non-used imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,11 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from webdriver_manager.firefox import GeckoDriverManager
 from curl import *
 
 
 @pytest.fixture(scope="function")
 def driver():
     driver = webdriver.Firefox()
-    driver.set_window_size(1280, 720)
     driver.get(main_site)
     yield driver
     driver.quit()
